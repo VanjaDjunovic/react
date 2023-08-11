ec11f86f3f2f8916bd6fb0a97d13d82ee31ead78
vanja0034 <vanja.djunovic9@gmail.com>
2023-08-11 20:33:52 +0200

    Dodata stranica za prikaz jedne narudzbine

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProductPage from './components/ProductPage'
 import OrdersPage from './components/OrdersPage';
+import OrderShowPage from './components/OrderShowPage';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/products' element={<ProductPage />} />
+        <Route path='/orders/:id' element={<OrderShowPage />} />
         <Route path='*' element={<OrdersPage />} />
       </Routes>
     </BrowserRouter>
--- src/api.js
+++ src/api.js
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const URL = 'http://localhost:8000/api'
 
+export async function getAllProducts() {
+    const res = await axios.get(URL + '/products');
+    return res.data;
+}
+
 export async function getOrders(email = '', page = 1, size = 20) {
     const res = await axios.get(URL + '/orders', {
         params: {
